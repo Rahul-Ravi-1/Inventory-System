81adaf617173b6b9d6d05977ee38890a9aa7eb74
Rahul Ravi <rahulravics171@gmail.com>
2026-05-15 13:05:50 -0700

    Basic React + file structure set up

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import InventorySlot from "./InventorySlot";
+
+export default function App() {
+  return (
+    <main>
+      <h1>Inventory</h1>
+
+      <div className="inventory-box">
+        <InventorySlot/>
+      </div>
+    </main>
+  )
+}
---
+++ src/InventorySlot.jsx
@@ -0,0 +1,5 @@
+import './index.css';
+
+export default function InventorySlot({}) {
+    return <button type="button" className="inventory-slot" />;
+}
